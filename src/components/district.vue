aaf098988130d21c83ee5e2a7617a52ec68df34e
Knovour <knovour@gmail.com>
2016-12-14 13:29:05 +0800

    :art: turn space indent to tab

--- src/components/district.vue
+++ src/components/district.vue
@@ -1,26 +1,26 @@
 <template>
-  <select :class="[className, 'district']" :style="style" :name="name" v-model="selected" @change="$emit('change', $event.target.value)">
-    <option v-for="district in districts">{{ district }}</option>
-  </select>
+	<select :class="[className, 'district']" :style="style" :name="name" v-model="selected" @change="$emit('change', $event.target.value)">
+		<option v-for="district in districts">{{ district }}</option>
+	</select>
 </template>
 
 <script>
-  export default {
-    props: {
-      className: { type: String },
-      name:      { type: String, default: 'district' },
-      value:     { type: String },
-      districts: { type: Array },
-    },
-    computed: {
-      style() {
-        return {
-          width: 'calc((100% - 8px) / 3)'
-        };
-      },
+	export default {
+		props: {
+			className: { type: String },
+			name:      { type: String, default: 'district' },
+			value:     { type: String },
+			districts: { type: Array },
+		},
+		computed: {
+			style() {
+				return {
+					width: 'calc((100% - 8px) / 3)'
+				};
+			},
 			selected() {
 				return this.value;
 			}
-    },
-  };
+		},
+	};
 </script>
